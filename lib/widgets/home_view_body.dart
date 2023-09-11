8a89909d5ea60c82789c367a4640cd690b385abf
ramywael <ramybakry9@gmail.com>
2023-09-11 21:08:13 +0300

    Fix the Bug

--- lib/widgets/home_view_body.dart
+++ lib/widgets/home_view_body.dart
@@ -1,10 +1,7 @@
-
 import 'package:flutter/material.dart';
 import 'package:weather_api/widgets/container_item.dart';
-
 import 'custom_text.dart';
 import 'list_view_home.dart';
-import 'list_view_home_item.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({Key? key}) : super(key: key);
@@ -21,16 +18,17 @@ class HomeViewBody extends StatelessWidget {
               padding:  EdgeInsets.only(left: 20),
               child: CustomText(
                   text: "Today",
-                  color: Colors.white,
+                  color: Colors.black,
                   sizeFont: 25,
                   ),
             ),
             TextButton(onPressed:() {
 
-            },  child: const CustomText(
+            },
+                child: const CustomText(
                 text: "Forecasts",
-                color: Colors.white,
-                sizeFont: 15,
+                color: Colors.blueAccent,
+                sizeFont: 20,
                 )
             ),
           ],
