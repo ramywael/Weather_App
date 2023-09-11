5c4e1a542d5662295fb0eba48909fe60947a7e0c
ramywael <ramybakry9@gmail.com>
2023-09-11 20:21:42 +0300

    Refactor the HomeContainer

---
+++ lib/widgets/container_item.dart
@@ -0,0 +1,189 @@
+
+
+import 'package:flutter/material.dart';
+
+class ContainerView extends StatelessWidget {
+  const ContainerView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(right: 20,left: 20,top: 85),
+      height: 545,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        gradient: LinearGradient(
+          colors: [
+            const Color(0xff5C88ED).withOpacity(.8),
+            const Color(0xff5C88ED),
+          ],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        ),
+      ),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 12, left: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Image.asset(
+                  "assets/menu.png",
+                  width: 40,
+                  height: 40,
+                ),
+                const Row(
+                  children: [
+                    Icon(
+                      Icons.location_on,
+                      color: Colors.white,
+                      size: 25,
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      "London",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Icon(
+                      Icons.arrow_downward_sharp,
+                      color: Colors.white,
+                      size: 25,
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 10),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(12),
+                    child: Image.asset(
+                      "assets/R.png",
+                      width: 40,
+                      height: 40,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Image.asset(
+            "assets/cloud.png",
+            width: 200,
+            height: 200,
+          ),
+          const Text(
+            "32°",
+            style: TextStyle(
+                color: Color(0xffA9CBEC),
+                fontSize: 50,
+                fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Text(
+            "Cloudy",
+            style: TextStyle(
+                color: Colors.white.withOpacity(.5),
+                fontSize: 25,
+                fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Text(
+            "Friday,Sep 10",
+            style: TextStyle(
+                color: Colors.white.withOpacity(.5),
+                fontSize: 15,
+                fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Divider(
+            color: Colors.white.withOpacity(.5),
+            thickness: 1,
+            indent: 30,
+            endIndent: 30,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 30,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    Image.asset(
+                      "assets/windspeed.png",
+                      width: 50,
+                      height: 50,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Text(
+                      "12 km/h",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Image.asset(
+                      "assets/humidity.png",
+                      width: 50,
+                      height: 50,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Text(
+                      "42%",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Image.asset(
+                      "assets/cloud.png",
+                      width: 50,
+                      height: 50,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Text(
+                      "20%",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
